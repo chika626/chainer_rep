1a522e334359bbadad73e0e16ed7f17cbf596ec9
chika626 <21084340+chika626@users.noreply.github.com>
2019-11-20 14:31:06 +0900

    自作セットの読み込み機構の整備

--- train_my_set.py
+++ train_my_set.py
@@ -2,4 +2,11 @@ from chainer.datasets import mnist
 
 train_val, test = mnist.get_mnist(withlabel=True, ndim=1)
 
+
+#自作データセット読み込み
+from chainer.datasets import LabeledImageDataset
+train = LabeledImageDataset('data/train/train_labels.txt', 'data/train/images')
+
+
+
 def __main__():
