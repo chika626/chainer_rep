fe5b8e47269a877c804d0152f75c35684d13b44f
chika <21084340+chika626@users.noreply.github.com>
2019-11-22 00:33:44 +0900

    いろいろひどい

--- train_my_set.py
+++ train_my_set.py
@@ -14,22 +14,78 @@ from chainer.training import extensions
 import train_network as Net
 import configparser
 
+# def train(network_object, batchsize=128, gpu_id=0, max_epoch=20, train_dataset=None, valid_dataset=None, test_dataset=None, postfix='', base_lr=0.01, lr_decay=None):
+
+#     # 1. Dataset
+
+#     # train = LabeledImageDataset('data/train/train_labels.txt', 'data/train/images')
+#     # valid = LabeledImageDataset('data/train/valid_labels.txt', 'data/valid/images')
+#     # #独自サイズに変更
+#     # def transform(in_data):
+#     #     img, label = in_data
+#     #     img = resize(img, (224, 224))
+#     #     return img, label
+#     # #image labelの順で入ってる
+#     # train = TransformDataset(train, transform)
+#     # valid = TransformDataset(valid, transform)
+
+#     # cifer10で学習(いちおう)
+#     if train_dataset is None and valid_dataset is None and test_dataset is None:
+#         train_val, test = cifar.get_cifar10()
+#         train_size = int(len(train_val) * 0.9)
+#         train, valid = chainer.datasets.split_dataset_random(train_val, train_size, seed=0)
+#     else:
+#         train, valid, test = train_dataset, valid_dataset, test_dataset
+
+#     # 2. Iterator
+#     train_iter = iterators.MultiprocessIterator(train, batchsize)
+#     valid_iter = iterators.MultiprocessIterator(valid, batchsize, False, False)
+
+#     # 3. Model
+#     net = L.Classifier(network_object)
+
+#     # 4. Optimizer
+#     # SDGじゃなくて Adam使う
+#     # optimizer = optimizers.MomentumSGD(lr=base_lr).setup(net)
+#     optimizer = optimizers.Adam(final_lr=base_lr).setup(net)
+#     # 重みの発散を抑える
+#     optimizer.add_hook(chainer.optimizer.WeightDecay(0.0005))
+
+#     # 5. Updater
+#     updater = training.StandardUpdater(train_iter, optimizer, device=gpu_id)
+
+#     # 6. Trainer
+#     trainer = training.Trainer(updater, (max_epoch, 'epoch'), out='{}_cifar10_{}result'.format(network_object.__class__.__name__, postfix))
+
+#     # 7. Trainer extensions
+#     trainer.extend(extensions.LogReport())
+#     trainer.extend(extensions.observe_lr())
+#     trainer.extend(extensions.Evaluator(valid_iter, net, device=gpu_id), name='val')
+#     trainer.extend(extensions.PrintReport(['epoch', 'main/loss', 'main/accuracy', 'val/main/loss', 'val/main/accuracy', 'elapsed_time', 'lr']))
+#     trainer.extend(extensions.PlotReport(['main/loss', 'val/main/loss'], x_key='epoch', file_name='loss.png'))
+#     trainer.extend(extensions.PlotReport(['main/accuracy', 'val/main/accuracy'], x_key='epoch', file_name='accuracy.png'))
+#     if lr_decay is not None:
+#         trainer.extend(extensions.ExponentialShift('lr', 0.1), trigger=lr_decay)
+    
+#     # print('# Minibatch-size: {}'.format(batchsize))
+#     # print('# epoch: {}'.format(max_epoch))
+#     # print('# gpu_id: {}'.format(gpu_id))
+#     # print('')
+#     trainer.run()
+#     del trainer
+
+#     # 8. Evaluation
+#     test_iter = iterators.MultiprocessIterator(test, batchsize, False, False)
+#     test_evaluator = extensions.Evaluator(test_iter, net, device=gpu_id)
+#     results = test_evaluator()
+#     print('Test accuracy:', results['main/accuracy'])
+
+#     return net
+
+
 def train(network_object, batchsize=128, gpu_id=0, max_epoch=20, train_dataset=None, valid_dataset=None, test_dataset=None, postfix='', base_lr=0.01, lr_decay=None):
 
     # 1. Dataset
-
-    # train = LabeledImageDataset('data/train/train_labels.txt', 'data/train/images')
-    # valid = LabeledImageDataset('data/train/valid_labels.txt', 'data/valid/images')
-    # #独自サイズに変更
-    # def transform(in_data):
-    #     img, label = in_data
-    #     img = resize(img, (224, 224))
-    #     return img, label
-    # #image labelの順で入ってる
-    # train = TransformDataset(train, transform)
-    # valid = TransformDataset(valid, transform)
-
-    # cifer10で学習(いちおう)
     if train_dataset is None and valid_dataset is None and test_dataset is None:
         train_val, test = cifar.get_cifar10()
         train_size = int(len(train_val) * 0.9)
@@ -45,10 +101,7 @@ def train(network_object, batchsize=128, gpu_id=0, max_epoch=20, train_dataset=N
     net = L.Classifier(network_object)
 
     # 4. Optimizer
-    # SDGじゃなくて Adam使う
-    # optimizer = optimizers.MomentumSGD(lr=base_lr).setup(net)
-    optimizer = optimizers.Adam(lr=base_lr).setup(net)
-    # 重みの発散を抑える
+    optimizer = optimizers.MomentumSGD(lr=base_lr).setup(net)
     optimizer.add_hook(chainer.optimizer.WeightDecay(0.0005))
 
     # 5. Updater
@@ -66,13 +119,6 @@ def train(network_object, batchsize=128, gpu_id=0, max_epoch=20, train_dataset=N
     trainer.extend(extensions.PlotReport(['main/accuracy', 'val/main/accuracy'], x_key='epoch', file_name='accuracy.png'))
     if lr_decay is not None:
         trainer.extend(extensions.ExponentialShift('lr', 0.1), trigger=lr_decay)
-    
-    print('Device: {}'.format(device))
-    print('# unit: {}'.format(args.unit))
-    print('# Minibatch-size: {}'.format(args.batchsize))
-    print('# epoch: {}'.format(args.epoch))
-    print('# gpu_id: {}'.format(gpu_id))
-    print('')
     trainer.run()
     del trainer
 
@@ -84,6 +130,7 @@ def train(network_object, batchsize=128, gpu_id=0, max_epoch=20, train_dataset=N
 
     return net
 
+
 def main():
     # chainer.cuda.set_max_workspace_size(512*1024*1024)
     # #chainer.cuda.set_max_workspace_size(512 * 1024 * 1024)
@@ -91,16 +138,16 @@ def main():
 
     ini = configparser.ConfigParser()
     ini.read('./config.ini', 'UTF-8')
-    batchsize = ini['batchsize']
-    max_epoch = ini['max_epoch']
-    gpu_id = ini['gpu_id']
-    postfix = ini['postfix']
-    baseh_lr = ini['base_lr']
-    lr_decay = ini['lr_decay']
+    _batchsize = int(ini['data']['batchsize'])
+    _max_epoch = int(ini['data']['max_epoch'])
+    _gpu_id = int(ini['data']['gpu_id'])
+    _postfix = ini['data']['postfix']
+    _base_lr = float(ini['data']['base_lr'])
+    _lr_decay = int(ini['data']['lr_decay'])
 
     # ResNet 多分動くはず
-    model = train(Net.ResNet(ResBlock), batch_size=batch_size , gpu_id=gpu_id, max_epoch=max_epoch, base_lr=base_lr, lr_decay=lr_decay)
-
+    #model = train(Net.DeepCNN(10), batchsize=batchsize , gpu_id=gpu_id,  max_epoch=max_epoch, postfix=postfix , base_lr=base_lr, lr_decay=lr_decay)
+    model = train(Net.DeepCNN(10), max_epoch=_max_epoch, base_lr=_base_lr, lr_decay=(_lr_decay,'epoch'))
 
 if __name__ == "__main__":
     main()
--- train_network.py
+++ train_network.py
@@ -1,6 +1,9 @@
 import chainer
 from chainer.datasets import mnist
 import chainer.links as L
+import math
+import cupy
+import chainer.cuda
 import chainer.functions as F
 from chainer import iterators
 #from chainercv.transforms import resize
@@ -24,7 +27,7 @@ class ConvBlock(chainer.Chain):
             h = F.dropout(h, ratio=0.25)
         return h
 
-class LinearBlock(chainer.Chain): 
+class LinearBlock(chainer.Chain):
 
     def __init__(self, drop=False):
         w = chainer.initializers.HeNormal()
@@ -38,7 +41,7 @@ class LinearBlock(chainer.Chain):
         if self.drop:
             h = F.dropout(h)
         return h
-
+        
 class DeepCNN(chainer.ChainList):
 
     def __init__(self, n_output):
@@ -64,65 +67,116 @@ class DeepCNN(chainer.ChainList):
 # ここからResNet
 # 他人実装ResNet、とりあえず回しながら改変
 
-class ResBlock(chainer.Chain):
-    def __init__(self,n_in,n_out,stride=1,ksize=1):
-        w = math.sqrt(2)
-        super(ResBlock,self).__init__(
-            conv1=L.Convolution2D(n_in,n_out,3,stride,1,w),
-            bn1=L.BatchNormalization(n_out),
-            conv2=L.Convolution2D(n_out,n_out,3,1,1,w),
-            bn2=BatchNormalization(n_out),
-        )
-    def __call__(self, x, train):
-        h = F.relu(self.bn1(self.conv1(x), test=not train))
-        h = self.bn2(self.conv2(h), test=not train)
-        if x.data.shape != h.data.shape:
-            xp = chainer.cuda.get_array_module(x.data)
-            n, c, hh, ww = x.data.shape
-            pad_c = h.data.shape[1] - c
-            p = xp.zeros((n, pad_c, hh, ww), dtype=xp.float32)
-            p = chainer.Variable(p, volatile=not train)
-            x = F.concat((p, x))
-            if x.data.shape[2:] != h.data.shape[2:]:
-                x = F.average_pooling_2d(x, 1, 2)
-        return F.relu(h + x)
+# class ResBlock(chainer.Chain):
+#     def __init__(self, n_in, n_out, stride=1, ksize=1):
+#         w = math.sqrt(2)
+#         super(ResBlock, self).__init__(
+#             conv1=L.Convolution2D(n_in, n_out, 3, stride, 1, w),
+#             bn1=L.BatchNormalization(n_out),
+#             conv2=L.Convolution2D(n_out, n_out, 3, 1, 1, w),
+#             bn2=L.BatchNormalization(n_out),
+#         )
+#     def __call__(self, x, train):
+#         h = F.relu(self.bn1(self.conv1(x), test=not train))
+#         h = self.bn2(self.conv2(h), test=not train)
+#         if x.data.shape != h.data.shape:
+#             xp = cupy.get_array_module(x.data)
+#             n, c, hh, ww = x.data.shape
+#             pad_c = h.data.shape[1] - c
+#             p = xp.zeros((n, pad_c, hh, ww), dtype=xp.float32)
+#             p = chainer.Variable(p, volatile=not train)
+#             x = F.concat((p, x))
+#             if x.data.shape[2:] != h.data.shape[2:]:
+#                 x = F.average_pooling_2d(x, 1, 2)
+#         return F.relu(h + x)
 
-class ResNet(chainer.Chain):
-    def __init__(self, block_class, n=18):
-        super(ResNet, self).__init__()
-        w = math.sqrt(2)
-        links = [('conv1', L.Convolution2D(3, 16, 3, 1, 0, w))]
-        links += [('bn1', L.BatchNormalization(16))]
-        for i in range(n):
-            links += [('res{}'.format(len(links)), block_class(16, 16))]
-        for i in range(n):
-            links += [('res{}'.format(len(links)),
-                       block_class(32 if i > 0 else 16, 32,
-                                   1 if i > 0 else 2))]
-        for i in range(n):
-            links += [('res{}'.format(len(links)),
-                       block_class(64 if i > 0 else 32, 64,
-                                   1 if i > 0 else 2))]
-        links += [('_apool{}'.format(len(links)),
-                   F.AveragePooling2D(6, 1, 0, False, True))]
-        links += [('fc{}'.format(len(links)),
-                   L.Linear(64, 10))]
-        for link in links:
-            if not link[0].startswith('_'):
-                self.add_link(*link)
-        self.forward = links
-        self.train = True
-    def __call__(self, x, t):
-        for name, f in self.forward:
-            if 'res' in name:
-                x = f(x, self.train)
-            else:
-                x = f(x)
-        if self.train:
-            self.loss = F.softmax_cross_entropy(x, t)
-            self.accuracy = F.accuracy(x, t)
-            return self.loss
-        else:
-            return x
+# class ResNet(chainer.Chain):
+#     def __init__(self, block_class, n=18):
+#         super(ResNet, self).__init__()
+#         w = math.sqrt(2)
+#         links = [('conv1', L.Convolution2D(3, 16, 3, 1, 0, w))]
+#         links += [('bn1', L.BatchNormalization(16))]
+#         for i in range(n):
+#             links += [('res{}'.format(len(links)), block_class(16, 16))]
+#         for i in range(n):
+#             links += [('res{}'.format(len(links)),
+#                        block_class(32 if i > 0 else 16, 32,
+#                                    1 if i > 0 else 2))]
+#         for i in range(n):
+#             links += [('res{}'.format(len(links)),
+#                        block_class(64 if i > 0 else 32, 64,
+#                                    1 if i > 0 else 2))]
+#         links += [('_apool{}'.format(len(links)),
+#                    F.AveragePooling2D(6, 1, 0, False, True))]
+#         links += [('fc{}'.format(len(links)),
+#                    L.Linear(64, 10))]
+#         for link in links:
+#             if not link[0].startswith('_'):
+#                 self.add_link(*link)
+#         self.forward = links
+#         self.train = True
+#     def __call__(self, x, t):
+#         for name, f in self.forward:
+#             if 'res' in name:
+#                 x = f(x, self.train)
+#             else:
+#                 x = f(x)
+#         if self.train:
+#             self.loss = F.softmax_cross_entropy(x, t)
+#             self.accuracy = F.accuracy(x, t)
+#             return self.loss
+#         else:
+#             return x
 
         
+
+# # 独自ResNet
+
+# class ResBlock(chainer.Chain):
+#     def __init__(self, n_in, n_out, stride=1, ksize=1):
+#         w = math.sqrt(2)
+#         super(ResBlock, self).__init__(
+#             conv1=L.Convolution2D(n_in, n_out, 3, stride, 1, w),
+#             bn1=L.BatchNormalization(n_out),
+#             conv2=L.Convolution2D(n_out, n_out, 3, 1, 1, w),
+#             bn2=L.BatchNormalization(n_out),
+#         )
+#     def __call__(self, x, train):
+#         h = F.relu(self.bn1(self.conv1(x), test=not train))
+#         h = self.bn2(self.conv2(h), test=not train)
+#         if x.data.shape != h.data.shape:
+#             xp = cupy.get_array_module(x.data)
+#             n, c, hh, ww = x.data.shape
+#             pad_c = h.data.shape[1] - c
+#             p = xp.zeros((n, pad_c, hh, ww), dtype=xp.float32)
+#             p = chainer.Variable(p, volatile=not train)
+#             x = F.concat((p, x))
+#             if x.data.shape[2:] != h.data.shape[2:]:
+#                 x = F.average_pooling_2d(x, 1, 2)
+#         return F.relu(h + x)
+
+# class ResBox(chainer.Chain):
+#     def __init__(self,n_out,n_last,stride=1,ksize=1):
+#         w = chainer.initializers.HeNormal()
+#         super(ResBox,self).__init__(
+#                 # ksize,stride,pad
+#                 # 畳み込み層 
+#                 # (1x1 conv equivalent)
+#                 conv1=L.Convolution2D(None,n_out, 1, 1, 0,initialW=w),
+#                 # (3x3 conv equivalent)
+#                 conv2=L.Convolution2D(n_out,n_out, 3, 1 ,1,initialW=w),
+#                 # (1x1 conv expansion)
+#                 conv2=L.Convolution2D(n_out,n_last, 1, 1 ,1,initialW=w),
+#                 # バッチ正規化
+#                 bn1=L.BatchNormalization(n_out),
+#                 bn2=L.BatchNormalization(n_out),
+#                 bn3=L.BatchNormalization(n_out)
+#         )
+#     def __call__(self,x):
+#         h=F.relu(self.bn1(self.conv1(x)))
+#         h=
+
+#          h = F.relu(self.bn(self.conv(x)))
+#         if self.pool_drop:
+#             h = F.max_pooling_2d(h, 2, 2)
+#             h = F.dropout(h, ratio=0.25)
