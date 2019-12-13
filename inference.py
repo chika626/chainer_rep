d96652a335337e0a60710b33d5e71f6ed529e223
chika626 <21084340+chika626@users.noreply.github.com>
2019-12-13 18:04:01 +0900

    なんかした

--- inference.py
+++ inference.py
@@ -1,42 +1,67 @@
-# まず同じネットワークのオブジェクトを作る
-infer_net = ResNet()
+import chainer
+import argparse
+from chainer import training
+from chainer.datasets import cifar
+from chainer.datasets import mnist
+import chainer.links as L
+import chainer.functions as F
+from chainer import iterators
+import numpy as np
+#from chainercv.transforms import resize
+from chainer.datasets import TransformDataset
+from chainer import optimizers
+from chainer.datasets import LabeledImageDataset
+from chainer.training import extensions
+import train_network as Net
+import data_set as dataset
+import configparser
+from chainer import serializers
+import datetime
 
-# そのオブジェクトに保存済みパラメータをロードする
-serializers.load_npz('my_mnist.model', infer_net)
 
-gpu_id = 0  # CPUで計算をしたい場合は、-1を指定してください
+def main():
+    infer_net = ResNet()
 
-if gpu_id >= 0:
-    infer_net.to_gpu(gpu_id)
+    # ここ変えて推論
+    model_name = ''
+    serializers.load_npz(model_name, infer_net)
 
-# 1つ目のテストデータを取り出します
-x, t = test[0]  #  tは使わない
 
-# どんな画像か表示してみます
-plt.imshow(x.reshape(28, 28), cmap='gray')
-plt.show()
+    ini = configparser.ConfigParser()  
+    ini.read('./config.ini', 'UTF-8')
+    gpu_id = int(ini['inference']['gpu_id'])
 
-# ミニバッチの形にする（複数の画像をまとめて推論に使いたい場合は、サイズnのミニバッチにしてまとめればよい）
-print('元の形：', x.shape, end=' -> ')
+    if gpu_id >= 0:
+        infer_net.to_gpu(gpu_id)
 
-x = x[None, ...]
+    # 1つ目のテストデータを取り出します
+    x, t = test[0]  #  tは使わない
 
-print('ミニバッチの形にしたあと：', x.shape)
+    # どんな画像か表示してみます
+    plt.imshow(x.reshape(28, 28), cmap='gray')
+    plt.show()
 
-# ネットワークと同じデバイス上にデータを送る
-x = infer_net.xp.asarray(x)
+    # ミニバッチの形にする（複数の画像をまとめて推論に使いたい場合は、サイズnのミニバッチにしてまとめればよい）
+    print('元の形：', x.shape, end=' -> ')
 
-# モデルのforward関数に渡す
-with chainer.using_config('train', False), chainer.using_config('enable_backprop', False):
-    y = infer_net(x)
+    x = x[None, ...]
 
-# Variable形式で出てくるので中身を取り出す
-y = y.array
+    print('ミニバッチの形にしたあと：', x.shape)
 
-# 結果をCPUに送る
-y = to_cpu(y)
+    # ネットワークと同じデバイス上にデータを送る
+    x = infer_net.xp.asarray(x)
 
-# 予測確率の最大値のインデックスを見る
-pred_label = y.argmax(axis=1)
+    # モデルのforward関数に渡す
+    with chainer.using_config('train', False), chainer.using_config('enable_backprop', False):
+        y = infer_net(x)
 
-print('ネットワークの予測:', pred_label[0])
+    # Variable形式で出てくるので中身を取り出す
+    y = y.array
+
+    # 結果をCPUに送る
+    y = to_cpu(y)
+
+    # 予測確率の最大値のインデックスを見る
+    pred_label = y.argmax(axis=1)
+
+    print('ネットワークの予測:', pred_label[0])
--- train_my_set.py
+++ train_my_set.py
@@ -18,73 +18,6 @@ import configparser
 from chainer import serializers
 import datetime
 
-# def train(network_object, batchsize=128, gpu_id=0, max_epoch=20, train_dataset=None, valid_dataset=None, test_dataset=None, postfix='', base_lr=0.01, lr_decay=None):
-
-#     # 1. Dataset
-
-#     # train = LabeledImageDataset('data/train/train_labels.txt', 'data/train/images')
-#     # valid = LabeledImageDataset('data/train/valid_labels.txt', 'data/valid/images')
-#     # #独自サイズに変更
-#     # def transform(in_data):
-#     #     img, label = in_data
-#     #     img = resize(img, (224, 224))
-#     #     return img, label
-#     # #image labelの順で入ってる
-#     # train = TransformDataset(train, transform)
-#     # valid = TransformDataset(valid, transform)
-
-#     # cifer10で学習(いちおう)
-#     if train_dataset is None and valid_dataset is None and test_dataset is None:
-#         train_val, test = cifar.get_cifar10()
-#         train_size = int(len(train_val) * 0.9)
-#         train, valid = chainer.datasets.split_dataset_random(train_val, train_size, seed=0)
-#     else:
-#         train, valid, test = train_dataset, valid_dataset, test_dataset
-
-#     # 2. Iterator
-#     train_iter = iterators.MultiprocessIterator(train, batchsize)
-#     valid_iter = iterators.MultiprocessIterator(valid, batchsize, False, False)
-
-#     # 3. Model
-#     net = L.Classifier(network_object)
-
-#     # 4. Optimizer
-#     # SDGじゃなくて Adam使う
-#     # optimizer = optimizers.MomentumSGD(lr=base_lr).setup(net)
-#     optimizer = optimizers.Adam(final_lr=base_lr).setup(net)
-#     # 重みの発散を抑える
-#     optimizer.add_hook(chainer.optimizer.WeightDecay(0.0005))
-
-#     # 5. Updater
-#     updater = training.StandardUpdater(train_iter, optimizer, device=gpu_id)
-
-#     # 6. Trainer
-#     trainer = training.Trainer(updater, (max_epoch, 'epoch'), out='{}_cifar10_{}result'.format(network_object.__class__.__name__, postfix))
-
-#     # 7. Trainer extensions
-#     trainer.extend(extensions.LogReport())
-#     trainer.extend(extensions.observe_lr())
-#     trainer.extend(extensions.Evaluator(valid_iter, net, device=gpu_id), name='val')
-#     trainer.extend(extensions.PrintReport(['epoch', 'main/loss', 'main/accuracy', 'val/main/loss', 'val/main/accuracy', 'elapsed_time', 'lr']))
-#     trainer.extend(extensions.PlotReport(['main/loss', 'val/main/loss'], x_key='epoch', file_name='loss.png'))
-#     trainer.extend(extensions.PlotReport(['main/accuracy', 'val/main/accuracy'], x_key='epoch', file_name='accuracy.png'))
-#     if lr_decay is not None:
-#         trainer.extend(extensions.ExponentialShift('lr', 0.1), trigger=lr_decay)
-    
-#     # print('# Minibatch-size: {}'.format(batchsize))
-#     # print('# epoch: {}'.format(max_epoch))
-#     # print('# gpu_id: {}'.format(gpu_id))
-#     # print('')
-#     trainer.run()
-#     del trainer
-
-#     # 8. Evaluation
-#     test_iter = iterators.MultiprocessIterator(test, batchsize, False, False)
-#     test_evaluator = extensions.Evaluator(test_iter, net, device=gpu_id)
-#     results = test_evaluator()
-#     print('Test accuracy:', results['main/accuracy'])
-
-#     return net
 
 
 def train(network_object, batchsize=128, gpu_id=0, max_epoch=30, train_dataset=None, valid_dataset=None, test_dataset=None, postfix='', base_lr=0.01, lr_decay=None,out_dump=true):
@@ -96,6 +29,17 @@ def train(network_object, batchsize=128, gpu_id=0, max_epoch=30, train_dataset=N
         train, valid = chainer.datasets.split_dataset_random(train_val, train_size, seed=0)
     else:
         train, valid, test = train_dataset, valid_dataset, test_dataset
+
+#     # train = LabeledImageDataset('data/train/train_labels.txt', 'data/train/images')
+#     # valid = LabeledImageDataset('data/train/valid_labels.txt', 'data/valid/images')
+#     # #独自サイズに変更
+#     # def transform(in_data):
+#     #     img, label = in_data
+#     #     img = resize(img, (224, 224))
+#     #     return img, label
+#     # #image labelの順で入ってる
+#     # train = TransformDataset(train, transform)
+#     # valid = TransformDataset(valid, transform)
 
     # 2. Iterator
     train_iter = iterators.MultiprocessIterator(train, batchsize)
@@ -118,8 +62,7 @@ def train(network_object, batchsize=128, gpu_id=0, max_epoch=30, train_dataset=N
     trainer.extend(extensions.LogReport())
     trainer.extend(extensions.observe_lr())
     trainer.extend(extensions.Evaluator(valid_iter, net, device=gpu_id), name='val')
-    #trainer.extend(extensions.PrintReport(['epoch', 'main/loss', 'main/accuracy', 'val/main/loss', 'val/main/accuracy', 'elapsed_time', 'lr']), call_before_training=True)
-    trainer.extend(extensions.PrintReport(['epoch', 'main/loss', 'main/accuracy', 'val/main/loss', 'val/main/accuracy', 'l1/W/data/std', 'elapsed_time']))
+    trainer.extend(extensions.PrintReport(['epoch', 'main/loss', 'main/accuracy', 'val/main/loss', 'val/main/accuracy', 'elapsed_time', 'lr']))
     trainer.extend(extensions.PlotReport(['main/loss', 'val/main/loss'], x_key='epoch', file_name='loss.png'))
     trainer.extend(extensions.PlotReport(['main/accuracy', 'val/main/accuracy'], x_key='epoch', file_name='accuracy.png'))
     # Print a progress bar to stdout
@@ -164,13 +107,8 @@ def main():
     _lr_decay = int(ini[s_0]['lr_decay'])
     _out_dump = bool(ini[s_0]['out_dump'])
 
-    # ResNet 多分動くはず
-    #odel = train(Net.DeepCNN(10), batchsize=_batchsize , gpu_id=_gpu_id,  max_epoch=_max_epoch, postfix=_postfix , base_lr=_base_lr, lr_decay=(_lr_decay,'epoch'))
-    #x = np.random.randn(1, 3, 32, 32).astype(np.float32)
-    #net = Net.ResNet(10)
-    #model = train(net(x), max_epoch=_max_epoch, base_lr=_base_lr, lr_decay=(_lr_decay,'epoch'))
-
     # これで動いてた
+    # ResNet(最終出力数)
     model = train(Net.ResNet(10),
      batchsize=_batchsize,
      gpu_id=_gpu_id
@@ -187,6 +125,7 @@ def main():
     #output model
     dt_now = datetime.datetime.now()
     serializers.save_npz('{}_{}_{}.model'.format(dt_now.year,dt_now.month,dt_now.day),model)
+
     
     
 
